43dec3e35bc24a67635414f4d028054eae30408f
Raamprakash.Soundaran <Raamprakash.Soundara@cntrline.com>
2019-03-14 22:25:35 -0400

    finall update day before practice

--- src/main/java/frc/robot/commands/DeployBallCollector.java
+++ src/main/java/frc/robot/commands/DeployBallCollector.java
@@ -9,9 +9,9 @@ public class DeployBallCollector extends CommandGroup{
     public DeployBallCollector(){
        addSequential(new WaitElevatorToPos(Constants.elevatorLiftToClearGrippersPos));
        addSequential(new WaitArm2ToPos(Constants.arm2CollectPos1));
-       //addParallel(new StartArmRollers());
-      // addSequential(new WaitArm1AndArm2ToPos(Constants.arm1collectPosFinal,Constants.arm2CollectPosfinal));  
-      // addSequential(new WaitElevatorToPos(Constants.elevatorLoadcargo));
-      // addSequential(new TurnOffElevatorMotor());
+       addParallel(new StartArmRollers());
+       addSequential(new WaitArm1AndArm2ToPos(Constants.arm1collectPosFinal,Constants.arm2CollectPosfinal));  
+       addSequential(new WaitElevatorToPos(Constants.elevatorLoadcargo));
+       addSequential(new TurnOffElevatorMotor());
     }
 }
